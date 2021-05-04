d264ec5a6e5544ce3a606322d94e9f0a91da6eae
Imran Sefat <imransefat@gmail.com>
2021-05-04 21:42:44 +0600

    project added

--- components/BlogThumbnail.js
+++ components/BlogThumbnail.js
@@ -1,19 +1,28 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function BlogThumbnail({ blog }) {
     const { title, slug, readingTime, thumbnail } = blog.fields
     return (
-        <div className="m-12 bg-blue-300 p-3 rounded-md shadow-xl cursor-pointer transition duration-300 transform hover:scale-110 group">
+        <div className="m-12  bg-blue-300 p-3 rounded-md shadow-xl transition duration-300 transform hover:scale-110 group">
             <Image src={'https:' + thumbnail.fields.file.url}
                 width={thumbnail.fields.file.details.image.width}
                 height={thumbnail.fields.file.details.image.height}
             />
-            <div className="text-3xl m-3">
+            <div className="text-3xl m-3 text-white">
                 {title}
             </div>
             <div className="text-xl m-3 text-gray-500 group-hover:animate-bounce">
                 {readingTime + " minutes reading"}
             </div>
+            <Link href={'/blogs/' + slug} >
+                <a>
+                    <div className="p-6 bg-gray-800 text-white text-center rounded-lg">
+                        Read Blog
+                    </div>
+                </a>
+            </Link>
+
 
         </div>
     )
---
+++ pages/blogs/[slug].js
@@ -0,0 +1,109 @@
+import Head from 'next/head'
+import { createClient } from 'contentful'
+import Image from 'next/image'
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+
+
+const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+})
+
+
+export default function Blogs({ blog }) {
+    const { title, readingTime, slug, featuredImage, tags, content } = blog.fields
+    const document = {
+        nodeType: 'document',
+        data: {},
+        content: [
+            {
+                nodeType: 'paragraph',
+                data: {},
+                content: [
+                    {
+                        nodeType: 'text',
+                        value: 'Hello world!',
+                        marks: [],
+                        data: {}
+                    },
+                ],
+            },
+        ],
+    }
+    console.log(content);
+    return (
+        <div>
+            <Head>
+                <title>{title} | Imran's Blog</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+
+            <div className="m-20 ">
+                <div className="flex flex-col items-center">
+                    <Image src={'https:' + featuredImage.fields.file.url}
+                        width={featuredImage.fields.file.details.image.width}
+                        height={featuredImage.fields.file.details.image.height}
+
+                    />
+                </div>
+
+
+                <div className="mt-20 mx-20 p-5">
+                    <p className="text-5xl font-serif my-4">{title}</p>
+                    <p className="text-xl  text-gray-500">{'Reading Time : ' + readingTime + ' minutes'}</p>
+
+                    <div className="flex">
+                        {tags.map(tag => (
+                            <div key={tag} className="bg-blue-400 rounded p-2 m-2">
+                                <p className="text-white">{tag}</p>
+                            </div>
+                        ))}
+                    </div>
+
+                    <div className="prose">
+                        {documentToReactComponents(content)}
+                    </div>
+                </div>
+
+
+
+
+            </div>
+
+        </div>
+
+    )
+}
+
+
+export async function getStaticProps({ params }) {
+    const { items } = await client.getEntries({
+        content_type: 'blog',
+        'fields.slug': params.slug
+
+    })
+    console.log(items[0]);
+    return {
+        props: {
+            blog: items[0]
+        }
+    }
+}
+
+export const getStaticPaths = async () => {
+    const res = await client.getEntries({
+        content_type: 'blog',
+        // 'fields.slug': params.slug
+    })
+    const paths = res.items.map(item => {
+        return {
+            params: { slug: item.fields.slug }
+        }
+    })
+
+    return {
+        paths,
+        fallback: false
+    }
+}
